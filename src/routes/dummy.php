b3a27bc3d860e59eda41e484195eea069a8745e6
JJavier Aristu <jose.aristu@momentumgames.io>
2023-10-11 14:57:32 +0200

    Added new route

--- src/routes/dummy.php
+++ src/routes/dummy.php
@@ -3,3 +3,7 @@
 Route::get('/api/dashboard/dummy', function () {
     return response()->json(['message' => '¡Hello!'], 200);
 });
+
+Route::get('/api/dashboard/dummy2', function () {
+    return response()->json(['message' => '¡Hello2!'], 200);
+});
